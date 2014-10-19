797af2bf041c07410897e86d093c4e63e184f420
phpSing <gosingk@gmail.com>
2014-10-19 17:16:25 +0800

    1
    1

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $_db = mysqli_connect($db['host'],$db['dbuser'],$db['dbpass'],$db['dbname']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1,user-scalable=no">	
  	<title>Bubble Attendence Sheet - Ruini Shi</title>
- 	<link rel="stylesheet" type="text/css" href="base.css">
+ 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
  	<link rel="stylesheet" type="text/css" href="style.css">
  </head>
  <body>
