706b1f8321da9938a5520fc4942db20dc9c34d13
phpSing <gosingk@gmail.com>
2014-10-19 23:07:09 +0800

    1
    1

--- bubble.js
+++ bubble.js
@@ -1,6 +1,19 @@
 (function($){
 
 
-	
+	$(document).ready(function() {
+		
+
+		$('.bubble-item').tap(function(e){
+			console.log('tapeed');
+			$(this).find('span.label').removeClass('label-primary').addClass('label-success').text('被捏了');
+		});
+
+
+
+
+
+
+	});
 
 })(jQuery)
