0cb08e56dc9d343273e936485b0e4c0985a16f15
phpSing <gosingk@gmail.com>
2014-10-19 17:10:55 +0800

    1
    1

--- index.php
+++ index.php
@@ -2,4 +2,25 @@
 /*!
 *	Bubble Attendence Sheet
 */
- ?>
+$db = array(
+	'host' => 'localhost',
+	'dbname' => 'bubble',
+	'dbuser' => 'root',
+	'dbpass' => 'root'
+	);
+$_db = mysqli_connect($db['host'],$db['dbuser'],$db['dbpass'],$db['dbname']);
+?>
+ <!doctype html>
+ <html lang="en">
+ <head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1,user-scalable=no">	
+ 	<title>Bubble Attendence Sheet - Ruini Shi</title>
+ 	<link rel="stylesheet" type="text/css" href="base.css">
+ 	<link rel="stylesheet" type="text/css" href="style.css">
+ </head>
+ <body>
+ <div class="container"></div>
+ </body>
+ </html>
